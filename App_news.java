3050aa62a088d581b13bf758994bd3292fc0fb74
Yumi-Park996 <byumm315@gmail.com>
2025-02-27 20:35:06 +0900

    Update App_news.java

--- App_news.java
+++ App_news.java
@@ -1,7 +1,7 @@
 public class App_news {
     public static void main(String[] args) {
         Monitoring_news monitoring = new Monitoring_news();
-        String htmlContent = monitoring.getNews(System.getenv("KEYWORD"), 10, 1, SortType.date);
+        String htmlContent = monitoring.getNews(System.getenv("KEYWORD"), 30, 1, SortType.date);
         System.out.println(htmlContent); // HTML을 출력하여 GitHub Actions에서 사용
     }
 }
